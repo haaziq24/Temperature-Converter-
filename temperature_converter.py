fc0cde5591e2049b2d207de43d425bf522e44f2e
Haaziq Abbasi <haaziq24@gmail.com>
2025-08-28 01:38:05 -0400

    Add files via upload

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,1 +1,3 @@
-
+fahrenheit = float(input('Enter the Tmeperature in Fahrenheit: '))
+celcius = (fahrenheit - 32) / 1.8
+print(f'{fahrenheit} degrees Fahrenehit is equal to {celcius:.2f} degrees Celcius')
